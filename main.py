6391ccf12c84862dc525168d82de7ce56059e9e4
toswald6 <triston.oswald@gatech.edu>
2023-07-28 11:01:56 -0400

    Variable basics

--- main.py
+++ main.py
@@ -3,6 +3,28 @@
 
 # Print statement
 print("Triston Oswald")
+
+# Create integer variable (store value "10" in memory)
+price = 10
+# Update price value to be 30
+price += 20
+# Define floating point value
+rating = 3.3
+# String variable
+name = 'Triston'
+# Boolean variable (note: boolean values are case-sensitive)
+boolean = False
+
+print(price)
+print(rating)
+print(name)
+print(boolean)
+
+
+
+
+
+
 # Print statement multiplier
 print('x5 ' * 5)
 # ASCII art
