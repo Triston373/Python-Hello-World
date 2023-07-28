fc02190999f84acb73af2a830f1ee42f4daf46d8
toswald6 <triston.oswald@gatech.edu>
2023-07-28 10:53:03 -0400

    Print statement experimentation

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-print("Triston Oswald")
+# Lab written by Triston Oswald, deviation from tutorials given
+# from Programming with Mosh here: https://www.youtube.com/watch?v=_uQrJ0TkZlc
 
+# Print statement
+print("Triston Oswald")
+# Print statement multiplier
+print('x5 ' * 5)
+# ASCII art
+print("""\
+                                       ._ o o
+                                       \_`-)|_
+                                    ,""       \ 
+                                  ,"  ## |   ಠ ಠ. 
+                                ," ##   ,-\__    `.
+                              ,"       /     `--._;)
+                            ,"     ## /
+                          ,"   ##    /
+                    """)
